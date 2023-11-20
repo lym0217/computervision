0530c2df27e67fc9eb90eed3fbc3bfa5b05acb0e
Yeongmin_Lee <65957439+lym0217@users.noreply.github.com>
2023-11-20 15:46:31 +0900

    Update and rename 2019203013_HW1_2.cpp to 컴퓨터비전/2019203013_HW1_2.cpp

--- 컴퓨터비전/2019203013_HW1_2.cpp
+++ 컴퓨터비전/2019203013_HW1_2.cpp
@@ -14,17 +14,17 @@ void main()
 	const double PI = 3.141592;
 
 	cin >> degree;
-	double rad = degree * PI / 180; // °¢µµ¸¦ radianÀ¸·Î º¯È¯
+	double rad = degree * PI / 180; // ê°ë„ë¥¼ radianìœ¼ë¡œ ë³€í™˜
 	double y_center = (double)img_input.cols / 2, x_center = (double)img_input.rows / 2;
 	int x_point, y_point;
 
 	for (int i = 0; i < img_input.rows; i++) {
 		for (int j = 0; j < img_input.cols; j++) {
-			//¹İ ½Ã°è ¹æÇâÀ¸·ÎÀÇ È¸Àü ÁÂÇ¥¸¦ °è»ê
+			//ë°˜ ì‹œê³„ ë°©í–¥ìœ¼ë¡œì˜ íšŒì „ ì¢Œí‘œë¥¼ ê³„ì‚°
 			x_point = (int)(y_center + ((double)i - x_center) * cos(rad) - ((double)j - y_center) * sin(rad));
 			y_point = (int)(x_center + ((double)i - x_center) * sin(rad) + ((double)j - y_center) * cos(rad));
 			if (x_point >= 0 && x_point < img_input.cols && y_point >= 0 && y_point < img_input.rows) {
-				// screenÀÇ ¹üÀ§¸¦ ³Ñ¾î°¡¸é ¿À·ù ¹ß»ıÇÏ¿© img_inputÀÇ ¹üÀ§ ³»¿¡ ÇØ´çÇÏ´Â °æ¿ì¿¡¸¸ ÀúÀå
+				// screenì˜ ë²”ìœ„ë¥¼ ë„˜ì–´ê°€ë©´ ì˜¤ë¥˜ ë°œìƒí•˜ì—¬ img_inputì˜ ë²”ìœ„ ë‚´ì— í•´ë‹¹í•˜ëŠ” ê²½ìš°ì—ë§Œ ì €ì¥
 				img_output.at<uchar>(x_point, y_point) = img_input.at<uchar>(i, j);
 			}
 		}
